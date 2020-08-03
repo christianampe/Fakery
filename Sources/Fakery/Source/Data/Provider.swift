895fe41d9edb7b67690fd3e5316950da31660242
Christian Ampe <christianampe.work@gmail.com>
2020-08-02 17:42:14 -0700

    Updated file path handling

--- Sources/Fakery/Source/Data/Provider.swift
+++ Sources/Fakery/Source/Data/Provider.swift
@@ -6,42 +6,22 @@ public final class Provider {
   // MARK: - Locale data
 
   public func dataForLocale(_ locale: String) -> Data? {
-    var translation: Data?
-
     if let translationData = translations[locale] {
-      translation = translationData
-    } else {
-        let bundle = Bundle.module
-
-      var path = bundle.path(forResource: locale,
-                             ofType: Config.pathExtension,
-                             inDirectory: Config.dirPath) ??
-                 bundle.path(forResource: locale,
-                             ofType: Config.pathExtension,
-                             inDirectory: Config.dirFrameworkPath)
-
-      if !Config.dirResourcePath.isEmpty {
-        path = "\(Config.dirResourcePath)/\(locale).\(Config.pathExtension)"
-      }
-
-      if let resourcePath = Bundle(for: Provider.self).resourcePath {
-        let bundlePath = resourcePath + "/Faker.bundle"
-
-        if let bundle = Bundle(path: bundlePath) {
-          path = bundle.path(forResource: locale, ofType: Config.pathExtension)
-        }
-      }
-
-      if let path = path {
-        let fileURL = URL(fileURLWithPath: path)
-
-        if let data = try? Data(contentsOf: fileURL) {
-          translation = data
-          translations[locale] = data
-        }
-      }
+        return translationData
     }
-
-    return translation
+    
+    let bundle = Bundle.module
+    
+    guard
+        let path = bundle.path(forResource: locale, ofType: Config.pathExtension),
+        let url = URL(string: path),
+        let data = try? Data(contentsOf: url)
+    else {
+        return nil
+    }
+    
+    translations[locale] = data
+    
+    return data
   }
 }
